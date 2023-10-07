029358f314c473f0272f186cba51674217f3b4dd
xmavv <xxmatheooo@gmail.com>
2023-10-08 00:05:42 +0200

    popup close by clicking on bg, esc, smooth scroll

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const link = document.querySelectorAll('.navigation__link');
-const popup = document.querySelectorAll('.popup');
+const popup = document.querySelector('.popup');
+const popupContent = document.querySelector('.popup__content');
+const popupClose = document.querySelector('.popup__close');
+const btnBookNow = document.querySelectorAll('.btn-book-now');
 
+// checkbox hack
 link.forEach( e => e.addEventListener('click', () => navToggle = document.getElementById('navi-toggle').checked = false));
 
-popup.addEventListener('click', () => { popup.classlist.add('show') });
-
-document.addEventListener('click', e => {
-    
+//showing popup
+btnBookNow.forEach(e => {
+    e.addEventListener('click', () => {
+        popup.classList.remove('u-display-none-popup');
+        popupContent.classList.remove('u-display-none-popup-content');
+    })
 })
 
-// jeszcze w popupie wyjscie
+//removing popup
+popup.addEventListener('click', () => {
+    popup.classList.add('u-display-none-popup');
+    popupContent.classList.add('u-display-none-popup-content');
+});
+
+popupClose.addEventListener('click', () => {
+    popup.classList.add('u-display-none-popup');
+    popupContent.classList.add('u-display-none-popup-content');
+})
+
+window.addEventListener('keydown', e => {
+    if (e.key === 'Escape'){
+        popup.classList.add('u-display-none-popup');
+        popupContent.classList.add('u-display-none-popup-content');
+    }
+});
+
+// do not close clicking inside content
+popupContent.addEventListener('click', e => e.stopPropagation())
